0293d6c34f60d09bb37921ca07bb37fc11db5085
Gustavo Renan Petry <grpetry@furb.br>
2025-11-14 21:05:48 -0300

    Teste1

--- src/main/java/com/exemplo/TrabalhoFinalGusIgoVin/App.java
+++ src/main/java/com/exemplo/TrabalhoFinalGusIgoVin/App.java
@@ -1,13 +1,10 @@
 package com.exemplo.TrabalhoFinalGusIgoVin;
 
-/**
- * Hello world!
- *
- */
+
 public class App 
 {
     public static void main( String[] args )
     {
-        
+       System.out.println("Igor está dando seta!!!"); 
     }
 }
