309c8f22c928e01e4d598506501561a4ce3bbb9b
Igor Gabriel de Sousa <igsousa@furb.br>
2025-11-14 21:30:19 -0300

    Primeiro Commit

--- src/main/java/com/exemplo/TrabalhoFinalGusIgoVin/App.java
+++ src/main/java/com/exemplo/TrabalhoFinalGusIgoVin/App.java
@@ -6,5 +6,6 @@ public class App
     public static void main( String[] args )
     {
        System.out.println("Igor está dando seta!!!"); 
+       System.out.println("Gustavo ta piscando o zero!!!!");
     }
 }
